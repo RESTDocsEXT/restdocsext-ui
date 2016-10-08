4c4311f0d000a75d72e26f3d6e8b66125cae8c86
Paul Samsotha <paulsamsotha.dev@gmail.com>
2016-10-08 19:52:28 +0700

    Add documentation and rest-client skeletons

---
+++ src/app/features/rest-client/rest-client.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { RestClientComponent } from './rest-client.component';
+
+describe('Component: RestClient', () => {
+  it('should create an instance', () => {
+    let component = new RestClientComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/rest-client/rest-client.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rest-client',
+  templateUrl: './rest-client.component.html',
+  styleUrls: ['./rest-client.component.scss']
+})
+export class RestClientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
