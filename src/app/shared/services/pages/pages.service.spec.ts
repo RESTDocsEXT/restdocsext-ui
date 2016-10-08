b59b58d936cd7ffcef3e7bb55314b503305ee85c
Paul Samsotha <paulsamsotha.dev@gmail.com>
2016-10-08 15:06:44 +0700

    Add service PagesService and tests

---
+++ src/app/shared/services/pages/pages.service.spec.ts
@@ -0,0 +1,53 @@
+import { async, inject, TestBed } from '@angular/core/testing';
+
+import { Http, Response, ResponseOptions, BaseRequestOptions } from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+import { PagesService } from './pages.service';
+
+describe('service: PagesService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        PagesService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http, useFactory: (backend, options) => {
+            return new Http(backend, options);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ]
+    });
+  });
+
+  it('should set the current page on success',
+      async(inject([PagesService, MockBackend], (service: PagesService, backend: MockBackend) => {
+
+    backend.connections.subscribe((conn: MockConnection) => {
+      let options = new ResponseOptions({ body: '<h2>Hello</h2>'});
+      conn.mockRespond(new Response(options));
+    });
+
+    service.currentPage$.subscribe(page => {
+      expect(page).toEqual('<h2>Hello</h2>');
+    });
+
+    service.currentPage = 'url';
+  })));
+
+  it('should set the current page on error',
+      async(inject([PagesService, MockBackend], (service: PagesService, backend: MockBackend) => {
+
+    backend.connections.subscribe((conn: MockConnection) => {
+      conn.mockError(new Error('<h2>Error</h2>'));
+    });
+
+    service.currentPage$.subscribe(page => {
+      expect(page).toContain('<h2>Error</h2>');
+    });
+
+    service.currentPage = 'url';
+  })));
+});
---
+++ src/app/shared/services/pages/pages.service.ts
@@ -0,0 +1,32 @@
+
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class PagesService {
+
+  private baseUrl = '/assets/mock-data/docs/';
+  private _currentPage = new ReplaySubject<string>(1);
+
+  constructor(private http: Http) {}
+
+  set currentPage(path: string) {
+    this.http.get(`${this.baseUrl + path}.html`)
+      .map(res => res.text())
+      .toPromise()
+      .then(content => {
+        this._currentPage.next(content);
+      })
+      .catch(error => {
+        this._currentPage.next(`Error processing page: ${error.message}`);
+      });
+  }
+
+  get currentPage$(): Observable<string> {
+    return this._currentPage.asObservable();
+  }
+}
