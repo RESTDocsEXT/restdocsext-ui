295536908782469b875bd9c1ef07b93aaa0c59d8
Paul Samsotha <paulsamsotha.dev@gmail.com>
2016-10-10 12:38:03 +0700

    Dont use barrels with documentation

---
+++ src/app/features/documentation/documentation.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+import { DocumentationComponent } from './documentation.component';
+
+@NgModule({
+  declarations: [ DocumentationComponent ],
+  exports: [ DocumentationComponent ]
+})
+export class DocumentationModule {}
